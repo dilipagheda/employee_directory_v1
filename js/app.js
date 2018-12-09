ef66298c16ceb558f855adb7116a089c064eaacb
Dilip <dilip_agheda@yahoo.com>
2018-12-09 14:00:18 +1100

    implemented search functionality and bug fixes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 let people;
 $('.overlay-container').hide();
 
-// data.results is an array of 12 objects reprsenting each person
+//data.results is an array of 12 objects reprsenting each person
 fetch("https://randomuser.me/api/?results=12&nat=au").then(result => result.json()).then(data => {
   people = data.results;
   display();
@@ -22,6 +22,8 @@ function display() {
     cardDiv.append(imageElement, detailsDiv);
     cardDiv.data("current", index);
     $('.container').append(cardDiv);
+    people[index].display = true;
+
   });
   $('.card').click(function() {
     let current = $(this).data("current");
@@ -44,19 +46,14 @@ function displayDetails(current) {
   let phone = $(`<span class="phone">${person.phone}</span>`);
   let address = $(`<span class="address">${person.location.street}</span>`);
   let state = $(`<span class="state">${person.location.state} ${person.location.postcode}</span>`);
-  let birthday = $(`<span class="birthday">Birthday:${person.dob.date}</span>`);
+  let birthday = $(`<span class="birthday">Birthday:${new Date(Date.parse(person.dob.date)).toLocaleDateString(navigator.language)}</span>`);
   let closeButton = $(`<i class="far fa-times-circle close"></i>`);
-  let nextButton = $(`<i class="far fa-arrow-alt-circle-right next"></i>`);
-  let previousButton = $(`<i class="far fa-arrow-alt-circle-left previous"></i>`);
 
   popupDiv.append(imageElement, name, email, city, line, phone, address, state, birthday, closeButton);
-  if (current + 1 < people.length) {
-    popupDiv.append(nextButton);
-  }
-  if (current - 1 >= 0) {
-    popupDiv.append(previousButton);
-  }
+
   $('.popup-container').append(popupDiv);
+  updateNextArrow(findNextVisibleCard(current));
+  updatePreviousArrow(findPreviousVisibleCard(current));
   $('.overlay-container').show();
   $('.close').click(function() {
     $('.popup-container').empty();
@@ -64,13 +61,82 @@ function displayDetails(current) {
   });
   $('.next').click(function() {
     $('.popup-container').empty();
-
-    displayDetails(current + 1);
+    let r = findNextVisibleCard(current);
+    updateNextArrow(r);
+    displayDetails(r);
   });
   $('.previous').click(function() {
     $('.popup-container').empty();
-    displayDetails(current - 1);
-
+    let r = findPreviousVisibleCard(current);
+    updatePreviousArrow(r);
+    displayDetails(r);
   });
+}
 
+function updateNextArrow(current){
+  let nextButton = $(`<i class="far fa-arrow-alt-circle-right next"></i>`);
+  let popupDiv = $('.popup');
+
+  if (current!==-1) {
+    popupDiv.append(nextButton);
+  }else{
+    $('.next').remove();
+  }
+
+}
+
+function updatePreviousArrow(current){
+  let previousButton = $(`<i class="far fa-arrow-alt-circle-left previous"></i>`);
+  let popupDiv = $('.popup');
+
+
+  if (current !== -1) {
+    popupDiv.append(previousButton);
+  }else{
+    $('.previous').remove();
+  }
+}
+
+function findPreviousVisibleCard(current){
+  for(let i=current-1;i>=0;i--){
+    if(people[i].display===true){
+      return i;
+    }
+  }
+  return -1;
+}
+
+function findNextVisibleCard(current){
+  for(let i=current+1;i<people.length;i++){
+    if(people[i].display){
+      return i;
+    }
+  }
+  return -1;
+}
+
+//Search functionality
+$("#txtSearch").on('keyup',function(){
+  let phrase = $(this).val();
+  $('.card').each(function(){
+    let name = $(this).find('.details').find('.name').text();
+    let $card = $(this);
+    if(name.includes(phrase)){
+      $card.show();
+    }else{
+      $card.hide();
+    }
+    updatePeople(phrase);
+  });
+});
+
+function updatePeople(name){
+  people.forEach((person,index)=>{
+    let currentName = `${person.name.first} ${person.name.last}`;
+    if(currentName.includes(name)){
+      people[index].display = true;
+    }else{
+      people[index].display = false;
+    }
+  });
 }
